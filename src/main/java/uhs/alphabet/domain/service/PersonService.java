2fa874e60c683e30eaeb9828781fedf8902166cf
Bue-von-hon <dkssudvn2@gmail.com>
2021-04-30 20:35:35 +0900

    null 갑스 해결
    @Autowired을 이용한 의존성 주입 대신 @RequiredArgsConstrucotr, 생성자 이용하여 주입

--- src/main/java/uhs/alphabet/domain/service/PersonService.java
+++ src/main/java/uhs/alphabet/domain/service/PersonService.java
@@ -12,7 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Service
 public class PersonService {
     private PersonRepository personRepository;
